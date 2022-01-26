0b50fedfca7d95ef6a6e8c8eaf3855dd908cc568
codewurld <sholaq@gmail.com>
2022-01-26 19:44:06 +0000

    added logic to get relevant information for each product and add to a list

--- shopify.py
+++ shopify.py
@@ -1,3 +1,4 @@
+from platform import release
 from turtle import title
 import requests
 import json
@@ -10,16 +11,43 @@ request = requests.get(url)
 #get shop data
 shopData = request.json()
 
+#stores list of returned products
+product_list = []
+
 #get data info
 for item in shopData['products']:
     title = item["title"]
     handle = item["handle"]
     productType = item["product_type"]
     releaseDate = item["created_at"]
-      #print(title, handle, productType, releaseDate)
+     
+    #get images - catch error if no img
+    for image in item['images']:
+        try:
+            imagesrc = image['src']
+        except:
+            imagesrc = 'None'
 
       #get variant data in products data
     for variant in item['variants']:
         price = variant['price']
         available = variant['available']
         print(price, available)
+         
+
+        #individual product 
+        product = {
+             'title': title,
+             'handle': handle,
+             'productType': productType,
+             'releaseDate': releaseDate,
+             'image': imagesrc,
+             'price': price,
+             'available': available
+
+         }
+
+        #add to list
+        product_list.append(product)
+
+print(product_list)
